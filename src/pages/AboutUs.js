1cda8306238f45e2d72ef2e7bfaed26dad3507ef
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-28 13:51:16 +0700

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import SrvicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <SrvicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
