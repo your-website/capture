64e7ebc62e33667b13113d0811e595b8fec3b1e8
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-28 14:31:32 +0700

    5. styled components for About Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import GlobalTyled from "./components/GlobalStyle";
+
 import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      <GlobalTyled />
     </div>
   );
 }
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,68 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
+
 const AboutSection = () => {
   return (
-    <div>
-      <div className="desctiption">
+    <About className="about">
+      <Description className="desctiption">
         <div className="title">
-          <div className="hide">
+          <Hide className="hide">
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide className="hide">
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide className="hide">
             <h2>true.</h2>
-          </div>
+          </Hide>
+          <p>
+            Contact us for any photography or videography ideas that yout have.
+            We have professionals with amazing skills.
+          </p>
         </div>
-        <p>
-          Contact us for any photography or videography ideas that yout have. We
-          have professionals with amazing skills.
-        </p>
         <button>Contact Us</button>
-        <div className="image">
-          <img src={home1} alt="" />
-        </div>
-      </div>
-    </div>
+      </Description>
+      <Image className="image">
+        <img src={home1} alt="" />
+      </Image>
+    </About>
   );
 };
+
+// Styled Component
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #fff;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
