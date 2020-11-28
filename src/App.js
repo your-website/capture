e6018068672a1b5aad4b6898f3066900562e3baf
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-28 15:47:07 +0700

    8. navigation component and style it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import GlobalTyled from "./components/GlobalStyle";
 
+import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
+
 function App() {
   return (
     <div className="App">
+      <Nav />
       <AboutUs />
       <GlobalTyled />
     </div>
