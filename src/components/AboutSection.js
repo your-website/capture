e69e1f18b5ae7503ace57fdcdca8755624f3962a
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-29 15:36:55 +0700

    12. add title animate

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,27 +3,53 @@ import home1 from "../img/home1.png";
 
 import { Layout, Description, Image, Hide } from "../styles";
 
+// Framer Motion
+import { motion } from "framer-motion";
+
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 1 } },
+  };
+
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        duration: 0.4,
+        ease: "easeOut",
+        staggerChildren: 0.3,
+        when: "beforeChildren",
+      },
+    },
+  };
+
   return (
     <Layout className="about">
       <Description className="desctiption">
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide className="hide">
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide className="hide">
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide className="hide">
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
           <p>
             Contact us for any photography or videography ideas that yout have.
             We have professionals with amazing skills.
           </p>
-        </div>
+        </motion.div>
         <button>Contact Us</button>
       </Description>
       <Image className="image">
