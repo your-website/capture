506996b2111a763666628c63f41bd5a725a5c9f3
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-29 18:47:00 +0700

    17. add faq animations

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import styled from "styled-components";
 import { Layout } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
   return (
@@ -9,46 +11,44 @@ const FaqSection = () => {
       <h2>
         Any questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start</h4>
-        <div className="answer">
-          <p>ac tincidunt vitae semper quis</p>
-          <p>
-            felis eget velit aliquet sagittis id consectetur purus ut faucibus
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>ac tincidunt vitae semper quis</p>
-          <p>
-            felis eget velit aliquet sagittis id consectetur purus ut faucibus
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Diferrent Payment Methods</h4>
-        <div className="answer">
-          <p>ac tincidunt vitae semper quis</p>
-          <p>
-            felis eget velit aliquet sagittis id consectetur purus ut faucibus
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What products do you offer.</h4>
-        <div className="answer">
-          <p>ac tincidunt vitae semper quis</p>
-          <p>
-            felis eget velit aliquet sagittis id consectetur purus ut faucibus
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              sed nunc sit amet magna iaculis pellentesque id in dui. Quisque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              sed nunc sit amet magna iaculis pellentesque id in dui. Quisque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Diferrent Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              sed nunc sit amet magna iaculis pellentesque id in dui. Quisque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What products do you offer.">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              sed nunc sit amet magna iaculis pellentesque id in dui. Quisque.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
