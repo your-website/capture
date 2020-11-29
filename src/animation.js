6e40028e9a9e4910e6dac09d7774e2e66394d715
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-29 16:26:34 +0700

    14. animations about components

--- src/animation.js
+++ src/animation.js
@@ -7,7 +7,9 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 0.6,
+      duration: 0.75,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
     },
   },
   exit: {
@@ -15,6 +17,37 @@ export const pageAnimation = {
     y: 300,
     transition: {
       duration: 0.4,
+    },
+  },
+};
+
+export const titleAnim = {
+  hidden: { y: 200 },
+  show: {
+    y: 0,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const fade = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { ease: "easeOut", duration: 0.75 },
+  },
+};
+
+export const photoAnim = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
     },
   },
 };
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,55 +5,31 @@ import { Layout, Description, Image, Hide } from "../styles";
 
 // Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
-  const titleAnim = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1, transition: { duration: 1 } },
-  };
-
-  const container = {
-    hidden: { x: 100 },
-    show: {
-      x: 0,
-      transition: {
-        duration: 0.4,
-        ease: "easeOut",
-        staggerChildren: 0.3,
-        when: "beforeChildren",
-      },
-    },
-  };
-
   return (
     <Layout className="about">
       <Description className="desctiption">
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="title"
-        >
-          <Hide className="hide">
-            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
-          </Hide>
-          <Hide className="hide">
-            <motion.h2 variants={titleAnim}>
-              your <span>dreams</span> come
-            </motion.h2>
-          </Hide>
-          <Hide className="hide">
-            <motion.h2 variants={titleAnim}>true.</motion.h2>
-          </Hide>
-          <p>
-            Contact us for any photography or videography ideas that yout have.
-            We have professionals with amazing skills.
-          </p>
-        </motion.div>
-        <button>Contact Us</button>
+        <Hide className="hide">
+          <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+        </Hide>
+        <Hide className="hide">
+          <motion.h2 variants={titleAnim}>
+            your <span>dreams</span> come
+          </motion.h2>
+        </Hide>
+        <Hide className="hide">
+          <motion.h2 variants={titleAnim}>true.</motion.h2>
+        </Hide>
+        <p>
+          Contact us for any photography or videography ideas that yout have. We
+          have professionals with amazing skills.
+        </p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image className="image">
-        <img src={home1} alt="" />
+        <motion.img variants={photoAnim} src={home1} alt="" />
       </Image>
     </Layout>
   );
